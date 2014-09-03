f63d699422c37d025a8350a0a7d66a0c65af3c9d
Patrick O'Brien <obiedman@gmail.com>
2014-09-03 13:18:21 -0400

    jsonp

--- app/controllers/trivia_questions_controller.rb
+++ app/controllers/trivia_questions_controller.rb
@@ -1,14 +1,14 @@
 class TriviaQuestionsController < ApplicationController
   def index
-    render json: TriviaQuestion.all
+    render json: TriviaQuestion.all, :callback => params[:callback]
   end
 
   def show
-    render json: TriviaQuestion.find(params[:id])
+    render json: TriviaQuestion.find(params[:id]), :callback => params[:callback]
   end
 
   def category
     questions = TriviaQuestion.where("category = ?", params[:category])
-    render :json => questions.shuffle.first(10)
+    render :json => questions.shuffle.first(10), :callback => params[:callback]
   end
 end
